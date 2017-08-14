57ee3449594fd37f08c5cfb8e9b4521448724999
206170 <704688407@qq.com>
2017-08-14 16:30:17 +0800

    测试

---
+++ app/src/main/java/com/example/a206170/order_system/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a206170.order_system;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
