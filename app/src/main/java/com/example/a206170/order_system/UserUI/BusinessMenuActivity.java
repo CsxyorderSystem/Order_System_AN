aa544f4f118a24ec0ebda072c072d503524bf453
Carlxzw <xiezhiweiisme@outlook.com>
2017-08-18 10:48:56 +0800

    v0.0.8 增加商家菜单界面

---
+++ app/src/main/java/com/example/a206170/order_system/UserUI/BusinessMenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.a206170.order_system.UserUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a206170.order_system.R;
+
+public class BusinessMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_menu);
+    }
+}
