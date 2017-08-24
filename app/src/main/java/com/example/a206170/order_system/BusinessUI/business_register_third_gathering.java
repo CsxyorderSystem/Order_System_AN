f9aeabc85eac7eefd154d1d69fe7a466cad8dc59
206170 <704688407@qq.com>
2017-08-24 15:21:56 +0800

    V 0.1.6 完成至认证签约人，下一步是实名认证

--- app/src/main/java/com/example/a206170/order_system/BusinessUI/business_register_third_gathering.java
+++ app/src/main/java/com/example/a206170/order_system/BusinessUI/business_register_third_gathering.java
@@ -1,11 +1,13 @@
 package com.example.a206170.order_system.BusinessUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -15,14 +17,15 @@ import com.example.a206170.order_system.R;
  * Created by 206170 on 2017/8/23.
  */
 
-public class businesss_register_second_gathering  extends AppCompatActivity {
+public class business_register_third_gathering extends AppCompatActivity {
     TextView openingbank,branchbank;
     EditText gatheringname,gatheringcard;
     RadioButton toself,topublic;
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_business_register_second_gathering);
+        setContentView(R.layout.activity_business_register_third_gathering);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setHomeButtonEnabled(true);
@@ -35,7 +38,20 @@ public class businesss_register_second_gathering  extends AppCompatActivity {
         gatheringcard=(EditText)findViewById(R.id.gatheringcard);
         toself=(RadioButton)findViewById(R.id.toself);
         topublic=(RadioButton)findViewById(R.id.topublic);
+        next=(Button)findViewById(R.id.save);
         ///以上为实例化对象
+        //以下为监听button
+        View.OnClickListener listener=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.save:
+                    next();
+                    break;
+            }
+            }
+        };
+       next.setOnClickListener(listener);
     }
     //以下是设置返回按钮
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -45,5 +61,10 @@ public class businesss_register_second_gathering  extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    //以下是跳转页面按钮的实现
+    public void next(){
+        Intent intent=new Intent(business_register_third_gathering.this,business_register_contract.class);
+        startActivity(intent);
     }
 }
